36143766c4392c43a110acdcd7910b3ab5edaafe
MatteaAnn Swain <MMSwain@tql.com>
2021-06-23 10:27:07 -0400

    morning session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showPassword: boolean = true;
+  user: any;
+  showPassword: boolean = false;
+  statusDisabled: boolean = true;//user.isAdmin;
+  image: string = "assets/images/Mia.jpg";
+  mia : boolean = false;
+  toggleImage(): void
+  {
+    if(this.mia){
+      this.image = "assets/images/Mia.jpg";
+    }
+    else{
+      this.image = "assets/images/mountains.jpg";
+    }
+    this.mia = !this.mia;
+  }
+
 }
